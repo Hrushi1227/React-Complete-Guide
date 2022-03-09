d9ef01b15062ffd6f83cafed8b519c3a2f257872
rushikesh <rushi.ghatol@gmail.com>
2022-03-10 02:14:03 +0530

    React latest meeting web application

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import Navigation from "./Components/Common/Navigations/Navigation";
 import Register from "./Components/Pages/Register/Register";
+import Login from "./Components/Pages/Login/Login";
 import "./App.css";
 
 function App() {
@@ -15,6 +16,9 @@ function App() {
           </Route>
           <Route path="/register">
             <Register />
+          </Route>
+          <Route path="/login">
+            <Login />
           </Route>
         </Switch>
       </BrowserRouter>
